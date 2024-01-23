c10ef6dc3c6406161a10a409b19bc66f9c334ab1
Ubaid-Nadeem <ubaidmuhammad916@gmail.com>
2024-01-23 06:40:34 +0500

    add model form

--- app.js
+++ app.js
@@ -78,17 +78,21 @@ function showService() {
 
 let mybutton = document.getElementById("bottom");
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
 }
 
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    window.scrollBy({
+        top: 0,
+        behavior: 'smooth'
+    });
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
